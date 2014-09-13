cc2f05066f23dbe4ab25ccf22799e8653811c2aa
Osana Osaretin <osana.osaretin@gmail.com>
2014-09-13 14:25:09 -0400

    Create AppDelegate.h

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Silly Story
+//
+//  Created by osaretin on 7/22/14.
+//  Copyright (c) 2014 ozone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UIViewController *viewController;
+
+@end
